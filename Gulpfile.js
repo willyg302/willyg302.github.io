969f0afc51412314a6208aceaa7a438f76f20cab
William Gaul <willyg302@gmail.com>
2015-01-14 10:57:43 -1000

    Moved to using Jade for templating

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,12 @@
 var gulp       = require('gulp');
+var jade       = require('gulp-jade');
 var less       = require('gulp-less');
 var minifycss  = require('gulp-minify-css');
 var uglify     = require('gulp-uglify');
 
 var browserify = require('browserify');
 var del        = require('del');
+var fs         = require('fs');
 var buffer     = require('vinyl-buffer');
 var vinyl      = require('vinyl-source-stream');
 
@@ -16,7 +18,9 @@ var paths = {
 	app: './app',
 	dist: './dist',
 	css: './app/less/main.less',
-	js: './app/js/main.js'
+	js: './app/js/main.js',
+	index: './app/jade/index.jade',
+	templateData: './app/jade/data.json'
 };
 
 gulp.task('clean', function(cb) {
@@ -44,6 +48,14 @@ gulp.task('compile-js', function() {
 		.pipe(gulp.dest(paths.dist));
 });
 
+gulp.task('compile-jade', function() {
+	return gulp.src(paths.index)
+		.pipe(jade({
+			data: JSON.parse(fs.readFileSync(paths.templateData))
+		}))
+		.pipe(gulp.dest('./'));
+});
+
 gulp.task('default', ['clean'], function() {
-	gulp.start('copy-assets', 'compile-css', 'compile-js');
+	gulp.start('copy-assets', 'compile-css', 'compile-js', 'compile-jade');
 });
