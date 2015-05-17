c43c6fa8b15275cc9fd8947f224e31365a7e47f5
William Gaul <willyg302@gmail.com>
2015-05-16 20:13:50 -0700

    Updated resume

--- okfile.py
+++ okfile.py
@@ -1,7 +1,13 @@
 project = 'WillyG Productions Site'
 
+def resume():
+	'''Build resume PDF'''
+	with ok.root('resume'):
+		ok.run('pdflatex William_Gaul_CV.tex')
+
 def install():
 	ok.npm('install').bower('install', root='app')
 
 def default():
+	'''Build the site'''
 	ok.node('gulp', module=True)
